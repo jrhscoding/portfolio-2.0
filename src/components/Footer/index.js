8297d8574a63f57e936a1df0387e34698469d32f
joseph <j.hernandez.92189@gmail.com>
2022-11-27 16:58:15 -0800

    added links to linkedin and twitter

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,9 @@ function Footer() {
     return (
         <footer className="bg-light text-center text-lg-start fixed-bottom">
             <div className="text-center p-3">
-                <a className="text-dark" href="https://github.com/jrhscoding">GitHub</a>
+                <a className="text-dark" href="https://github.com/jrhscoding">GitHub </a>
+                <a className="text-dark" href="https://www.linkedin.com/in/joseph-hernandez-8694b5255/">  LinkedIn  </a>
+                <a className="text-dark" href="https://twitter.com/livelovejosiee">Twitter</a>
             </div>
         </footer>
     )
